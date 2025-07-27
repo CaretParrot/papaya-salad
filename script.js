714c0e2ede730c3e5153ee6d71cbcf77b0794aea
CaretParrot <wopow29@gmail.com>
2025-07-26 22:06:37 -0500

    Fix icon and stylesheet links in index.html; adjust color palette values in script.js

--- script.js
+++ script.js
@@ -149,12 +149,12 @@ const randomPlus = {
 const colorPalletes = {
     savedColor: 0,
     paint: function (color = randomPlus.randomNumber(0, 360)) {
-        document.documentElement.style.setProperty(`--dark-1`, `hsl(${color}, 5%, 10%)`);
+        document.documentElement.style.setProperty(`--dark-1`, `hsl(${color}, 5%, 15%)`);
         document.documentElement.style.setProperty(`--dark-2`, `hsl(${color}, 10%, 20%)`);
-        document.documentElement.style.setProperty(`--dark-3`, `hsl(${color}, 15%, 30%)`);
-        document.documentElement.style.setProperty(`--light-1`, `hsl(${color}, 45%, 90%)`);
-        document.documentElement.style.setProperty(`--light-2`, `hsl(${color}, 40%, 80%)`);
-        document.documentElement.style.setProperty(`--light-3`, `hsl(${color}, 35%, 70%)`);
+        document.documentElement.style.setProperty(`--dark-3`, `hsl(${color}, 15%, 25%)`);
+        document.documentElement.style.setProperty(`--light-1`, `hsl(${color}, 75%, 85%)`);
+        document.documentElement.style.setProperty(`--light-2`, `hsl(${color}, 70%, 80%)`);
+        document.documentElement.style.setProperty(`--light-3`, `hsl(${color}, 65%, 75%)`);
         colorPalletes.savedColor = color;
     }
 }
